787dea1a31f04666ff7917e9a4f4462505f249e3
yil8 <yil8@uci.edu>
2015-02-08 12:50:20 -0800

    On branch master

--- src/crux/Test.java
+++ src/crux/Test.java
@@ -1,5 +1,7 @@
 package crux;
 
+import java.util.HashSet;
+
 import crux.Token.Kind;
 
 public class Test {
@@ -8,5 +10,16 @@ public class Test {
 		for (Kind tok: Token.Kind.values()) {
 			System.out.println(tok.name());
 		}
+		HashSet<Token.Kind> h;
+		
+		h = new HashSet<Token.Kind>();
+		h.add(Token.Kind.IDENTIFIER);
+//		{
+//	        private static final long serialVersionUID = 1L;
+//	        {
+//		        add(Token.Kind.IDENTIFIER);
+//		    }
+//	    };
+	    System.out.println(h);
 	}	
 }
